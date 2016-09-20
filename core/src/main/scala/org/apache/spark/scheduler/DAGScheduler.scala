c0c9744c9ddfe9dbf8e538786836be47b5969bce
Matteo Gazzetta <matteo_gazzetta@hotmail.it>
2016-09-20 17:04:05 +0200

    [DAG] Fix long to double

--- core/src/main/scala/org/apache/spark/scheduler/DAGScheduler.scala
+++ core/src/main/scala/org/apache/spark/scheduler/DAGScheduler.scala
@@ -1087,7 +1087,7 @@ class DAGScheduler(
         listenerBus.post(SparkStageWeightSubmitted(stage.latestInfo, properties,
           stageJson.asJsObject.fields("weight").convertTo[Long],
           stageJson.asJsObject.fields("parentsIds").convertTo[List[Int]],
-          stageJson.asJsObject.fields("nominalrate").convertTo[Long],
+          stageJson.asJsObject.fields("nominalrate").convertTo[Double],
           stageJson.asJsObject.fields("genstage").convertTo[Boolean]))
       }
       else {
