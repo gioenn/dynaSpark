2af55c77ea7e1e2f7856d57ee06563670c154f52
Matteo Gazzetta <matteo_gazzetta@hotmail.it>
2016-10-26 18:23:34 +0200

    [CONTROL] Removed hybrid heuristic

--- core/src/main/scala/org/apache/spark/scheduler/DAGScheduler.scala
+++ core/src/main/scala/org/apache/spark/scheduler/DAGScheduler.scala
@@ -260,8 +260,7 @@ class DAGScheduler(
 
         // COMPUTE DEADLINE
         val duration = stageJson.fields("duration").convertTo[Double]
-        val weight = average(List(stageJsonIds.size,
-          (totalDuration / duration) - 1))
+        val weight = (totalDuration / duration) - 1)
         val deadlineStage = controller.computeDeadlineStage(weight, currentTime, alpha, deadline)
 
         // UPDATE RECORD AND APP STATE
