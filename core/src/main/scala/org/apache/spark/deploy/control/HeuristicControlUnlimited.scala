3a1e303c1a1bf5c80c05f4362f6d5c2dca71a21e
Giovanni Quattrocchi <giovanniquattrocchi@me.com>
2018-05-14 21:22:31 +0200

    temporary fix to overscale

--- core/src/main/scala/org/apache/spark/deploy/control/HeuristicControlUnlimited.scala
+++ core/src/main/scala/org/apache/spark/deploy/control/HeuristicControlUnlimited.scala
@@ -7,6 +7,42 @@ import org.apache.spark.SparkConf
   */
 class HeuristicControlUnlimited(conf: SparkConf) extends HeuristicControl(conf) {
 
+
+  override def computeCores(coresToBeAllocated: Double,
+                            executorIndex: Int,
+                            stageId: Int,
+                            last: Boolean): (Double, Double, Double) = {
+    // compute core to start
+    val coreForExecutors = computeCoreForExecutors(coresToBeAllocated, stageId, last)
+
+    var coreToStart = coreForExecutors(executorIndex)
+
+    // compute max core
+    val coreMax = coreForVM
+
+    // return result
+    (coreMin, coreMax, coreToStart)
+  }
+
+  override def computeCoreForExecutors(coresToBeAllocated: Double, stageId: Int, last: Boolean): IndexedSeq[Double] = {
+
+    numExecutor = math.ceil(coresToBeAllocated.toDouble / coreForVM.toDouble).toInt
+
+    var coresToStart = 0
+    if (coresToBeAllocated == coreForVM*numMaxExecutor) {
+      coresToStart = math.ceil(coresToBeAllocated.toDouble).toInt
+    }
+    else {
+      coresToStart = math.ceil(coresToBeAllocated.toDouble / OVERSCALE).toInt
+    }
+    numExecutor = numMaxExecutor
+    (1 to numMaxExecutor).map { x =>
+        math.ceil((coresToStart / numExecutor.toDouble) / CQ) * CQ
+    }
+
+  }
+
+
   override def computeCoreStage(deadlineStage: Long = 0L, numRecord: Long = 0L, stageId: Int = 0, firstStage : Boolean = false, lastStage: Boolean = false): Double = {
     // if requested cores are greater than the available ones (coreForVM * numMaxExecutor), we fix the value to the
     // available ones
