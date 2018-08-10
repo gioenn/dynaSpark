bdb56279875116a32860d3b80c0fcde1b9f05289
Davide <davide.bertolotti@mail.polimi.it>
2018-08-10 17:50:07 +0200

    Fix.

--- core/src/main/scala/org/apache/spark/scheduler/DAGScheduler.scala
+++ core/src/main/scala/org/apache/spark/scheduler/DAGScheduler.scala
@@ -266,8 +266,7 @@ class DAGScheduler(
   /**
    * Called by the TaskSetManager to report task's starting.
    */
-  def worstCaseProfile[jsValue](appJJ:spray.json.jsValue , 
-      valExFlows:java.util.ArrayList[Integer] = null) {
+  def worstCaseProfile[jsValue](appJJ, valExFlows:java.util.ArrayList[Integer] = null) {
     var setP = appJJ.asJsObject.fields
     if (valExFlows != null) 
       setP.filter({case (k,v) => valExFlows.exists(x => x == k.toInt)})
