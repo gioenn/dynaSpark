e28278e88c329f6849d5e870f7082409bc638d08
Davide <davide.bertolotti@mail.polimi.it>
2018-10-28 16:56:50 +0100

    [HeuristicSymExControlUnlimited] fix: not to count skipped stages.

--- core/src/main/scala/org/apache/spark/deploy/control/HeuristicSymExControlUnlimited.scala
+++ core/src/main/scala/org/apache/spark/deploy/control/HeuristicSymExControlUnlimited.scala
@@ -31,10 +31,12 @@ class HeuristicSymExControlUnlimited(conf: SparkConf) extends HeuristicControlUn
     if (valExFlows != null) 
       setP = setP.filter({case (k,v) => valExFlows.exists(x => x == k.toInt)})
     var wCaseProfId = setP.keys.toList.zip(setP.toList.map(
-                      {case (k, v) => v.asJsObject.fields.count(_ => true)}))
-                      .filter({case (id, ns) => ns == setP.toList.map(
+                      {case (k, v) => v.asJsObject.fields
+                        .filter({case (k, v) => !v.asJsObject.fields("skipped").convertTo[Boolean]}) 
+                        .count(_ => true)})
+                      ).filter({case (id, ns) => ns == setP.toList.map(
                           {case (k, v) => v.asJsObject.fields
-                            //.filter(x => !x.asJsObject.fields("skipped").convertTo[Boolean])
+                            .filter({case (k, v) => !v.asJsObject.fields("skipped").convertTo[Boolean]})
                             .count(_ => true)}).max})(0)._1
     println("Worst case json profile number: " + wCaseProfId)
     setP(wCaseProfId)
