d4f45ca16aa763297a00f851d33f6f92f31d6b75
Davide <davide.bertolotti@mail.polimi.it>
2019-01-15 09:55:37 +0100

    Fix

--- core/src/main/scala/org/apache/spark/scheduler/SparkListener.scala
+++ core/src/main/scala/org/apache/spark/scheduler/SparkListener.scala
@@ -47,7 +47,7 @@ case class SparkStageWeightSubmitted
 (stageInfo: StageInfo, properties: Properties = null, weight: Long, duration: Long,
  totalduration: Long, parentsIds: List[Int],
  nominalrate: Double, genstage: Boolean, stageIds: List[String],
- executedstagesduration: Long = null)
+ executedstagesduration: Long = 0L)
   extends SparkListenerEvent
 
 @DeveloperApi
